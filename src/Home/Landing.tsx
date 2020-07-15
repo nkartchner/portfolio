fb151fbe25210c77fd71059d07e3e2af87c5aa39
Nathan Kartchner <nathan.kartchner@gmail.com>
2020-07-14 19:34:17 -0700

    fixed landing page videos

--- src/Home/Landing.tsx
+++ src/Home/Landing.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import clsx from "clsx";
+import myVideoMp4 from "./media/dyno.mp4";
+import myVideoWebM from "./media/dyno_converted.webm";
 import { makeStyles, createStyles, Theme } from "@material-ui/core";
 import Button from "@material-ui/core/Button";
 
@@ -86,12 +88,9 @@ const Landing: React.FC = () => {
                     disablePictureInPicture={true}
                     onLoadedData={handlePlay}
                 >
+                    <source src={myVideoWebM} type="video/webm" />
                     <source
-                        src="/public/media/dyno_converted.webm"
-                        type="video/webm"
-                    />
-                    <source
-                        src="/public/media/dyno.mp4"
+                        src={myVideoMp4}
                         type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"'
                     />
                 </video>
